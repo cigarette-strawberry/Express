115ae31a9a157deb2518a0eb5d38d88b86cf0f7c
XiaoWu <2467337566@qq.com>
2022-05-02 00:31:50 +0800

    ‘MySQL’

---
+++ controllers/defaultConfig.js
@@ -0,0 +1,13 @@
+//defaultConfig.js
+const config = {
+    // 数据库配置
+    database: {
+        DATABASE: 'my_app', //数据库名称
+        USERNAME: 'root', //mysql用户名
+        PASSWORD: 'wuchen0413', //mysql密码
+        PORT: '3306', //mysql端口号
+        HOST: '192.168.134.128' //服务器ip
+    }
+}
+
+module.exports = config
