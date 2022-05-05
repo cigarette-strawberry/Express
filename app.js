81167a5d3b8d214ebb5ea9f5243902cb47ad4eea
XiaoWu <2467337566@qq.com>
2022-05-05 22:22:25 +0800

    Token

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const parseJwt = require('express-jwt').expressjwt
+const SECRET_KEY = 'xiaowu2022' // 与生成token的密钥要一致!
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -15,27 +18,35 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    parseJwt({
+        secret: SECRET_KEY,
+        algorithms: ['HS256'], // 使用何种加密算法解析
+    }).unless({path: ['/login']}) // 登录页无需校验
+)
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const jwt = require('jsonwebtoken')
+const SECRET_KEY = 'xiaowu2022'
+
 // 引入数据库配置文件
 const userService = require('../controllers/mysqlConfig');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    // res.render('index', { title: 'Express' });
+router.post('/login', function (req, res, next) {
     const sql = 'SELECT * FROM users';
     userService.query(sql, (err, result) => {
         if (err) {
             return;
         }
-        // res：API传数据
-        // result：返回的数据，需要转成JSON格式
-        res.json(result);
+        // 校验密码....(此处省略), 如果校验成功, 生成jwt
+        // 参数1: 生成到token中的信息
+        // 参数2: 密钥
+        // 参数3: token的有效时间: 60, "2 days", "10h", "7d"
+        const token = jwt.sign(
+            {user: {name: 'xiaowu', password: 123456}},
+            SECRET_KEY,
+            {expiresIn: '3h'}
+        )
+        res.send({
+            status: 200,
+            message: 'login success!',
+            token,
+            data: result
+        })
     });
 });
 
