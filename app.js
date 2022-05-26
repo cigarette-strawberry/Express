bf68abb3866d04057efad02a3b316e04bf522d3a
morgan <2467337566@qq.com>
2022-05-26 21:06:14 +0800

    默认更改列表

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const rfs = require("rotating-file-stream");
+const createError = require('http-errors');
+const multiparty = require('multiparty');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
 
 const parseJwt = require('express-jwt').expressjwt
 const SECRET_KEY = 'xiaowu2022' // 与生成token的密钥要一致!
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+
+const logDirectory = path.join(__dirname, 'log')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+const generator = () => {
+    const today = new Date();
+    const year = today.getFullYear()
+    const month = today.getMonth() + 1
+    const day = today.getDate()
+    return `${year}-${month}-${day}.log`;
+};
+
+// 自定义token 日期格式
+morgan.token('localDate', function (req) {
+    let date = new Date();
+    return date.toLocaleString()
+})
+morgan.token('requestParameters', function (req) {
+    return JSON.stringify(req.query);
+})
+morgan.token('requestBody', function (req) {
+    return JSON.stringify(req.body);
+})
+morgan.token('requestFormData', function (req) {
+    const form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        console.log(fields)
+        return JSON.stringify(fields);
+    });
+})
+morgan.token('contentType', function (req) {
+    return req.headers['content-type'] || '无请求主体';
+})
+// 自定义format，其中包含自定义的token
+morgan.format('combined', ':remote-addr - :remote-user [:localDate] ":method :url HTTP/:http-version" :status :contentType :requestParameters :requestBody :requestFormData :res[content-length] ":referrer" ":user-agent"')
+
+const accessLogStream = rfs.createStream(generator, {
+    interval: '1d',
+    path: path.join(logDirectory)
+})
+
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use(
     parseJwt({
@@ -29,22 +69,37 @@ app.use(
     }).unless({path: ['/login']}) // 登录页无需校验
 )
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    // console.log(req, res)
+    const form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        console.log(req)
+        next()
+    });
+    // next()
+})
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler   捕获404并转发到错误处理程序
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// error handler   错误处理程序
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+    // set locals, only providing error in development   设置局部变量，仅提供开发中的错误
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // render the error page   呈现错误页面
     res.status(err.status || 500);
     res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,19 @@ const SECRET_KEY = 'xiaowu2022'
 // 引入数据库配置文件
 const userService = require('../controllers/mysqlConfig');
 
+router.get('/', (req, res, next) => {
+    res.send({
+        status: 200,
+        message: 'login success!',
+    })
+})
+
 router.post('/login', function (req, res, next) {
-    const sql = 'SELECT * FROM users';
+    const sql = 'SELECT * FROM T_Users';
     userService.query(sql, (err, result) => {
         if (err) {
-            return;
+            return err;
         }
-        // 校验密码....(此处省略), 如果校验成功, 生成jwt
-        // 参数1: 生成到token中的信息
-        // 参数2: 密钥
-        // 参数3: token的有效时间: 60, "2 days", "10h", "7d"
         const token = jwt.sign(
             {user: {name: 'xiaowu', password: 123456}},
             SECRET_KEY,
@@ -25,7 +28,7 @@ router.post('/login', function (req, res, next) {
         res.send({
             status: 200,
             message: 'login success!',
-            token,
+            token: 'Bearer ' + token,
             data: result
         })
     });
